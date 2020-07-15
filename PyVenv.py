ae9105d469d8490835f5b74297c9f0420aa91436
JRugambwa <67804210+JRugambwa@users.noreply.github.com>
2020-07-15 10:23:09 +0200

    Add files via upload

---
+++ PyVenv.py
@@ -0,0 +1,49 @@
+from flask import Flask, redirect, url_for, render_template, request
+app = Flask(__name__)
+import csv
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        with open('database.csv', newline='', mode='a') as database2:
+            csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_from():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('./thankyou.html')
+        except:
+            return 'did not save to database'
+
+
+    else:
+        return 'something went wrong. Try again!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
